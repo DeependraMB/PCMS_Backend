e1287611c3bd163e5eaf6f72a0b0da301e505a59
DeependraMB <deependramundalil2001@gmail.com>
2024-04-15 14:43:04 +0530

    DB Configured

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose
-  .connect(`${process.env.MONGODB_URL}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb+srv://deependra_mb:1zqjXClK9sVud5fw@pcms.fvrxyhv.mongodb.net/`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
-    console.log('\x1b[44m\x1b[33m%s\x1b[0m',"Connected to MongoDB");
+    console.log("\x1b[44m\x1b[33m%s\x1b[0m", "Connected to MongoDB");
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
